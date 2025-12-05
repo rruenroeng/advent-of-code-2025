cb92822a8d008480292685fdb89985da1fc0d9ee
Ryan Ruenroeng <rruenroeng@gmail.com>
2025-12-04 23:24:58 -0600

    Prune data for startup branch.

--- days/day01/part1.py
+++ days/day01/part1.py
@@ -2,23 +2,8 @@ def read_input(path="input.txt"):
     with open(path) as f:
         return [line.strip() for line in f]
 
-def solve(data):
-    current = 50
-    count = 0
-    for line in data:
-        direction = line[0]
-        magnitude = int(line[1:])
-        if direction == "L":
-            current = current - magnitude % 100
-        else:
-            current = current + magnitude % 100
-        if current < 0:
-            current + 100
-        current = current % 100
-        if(current) == 0:
-            count = count + 1
-        
-    return count
+def solve(data):       
+    return data[0]
 
 if __name__ == "__main__":
     inp = read_input()
